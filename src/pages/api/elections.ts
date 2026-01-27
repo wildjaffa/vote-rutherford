ad112072de3ca82338c38be00e9f3d354333aec5
wildjaffa <sungujdj@gmail.com>
2026-01-27 05:49:14 -0600

    code formatting and linting

--- src/pages/api/elections.ts
+++ src/pages/api/elections.ts
@@ -1,10 +1,15 @@
 import type { APIRoute } from "astro";
 import prisma from "../../lib/prisma";
+import type { ElectionWithRacesAndCandidates } from "../../lib/types";
 
 export const GET: APIRoute = async () => {
-  const elections = await prisma.election.findMany({});
+  const elections = await prisma.election.findMany({
+    include: { races: { include: { candidates: true } } },
+  });
 
   return new Response(JSON.stringify(elections), {
     headers: { "Content-Type": "application/json" },
   });
 };
+
+export type GetElectionsResponse = ElectionWithRacesAndCandidates[];
